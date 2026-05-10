f6a14b36a4c070735f530a101c8dd2335a42e53f
Syed Awais Wahab Shah <sawshah@users.noreply.huggingface.co>
2026-05-10 23:33:08 +0000

    Steps info

--- app.py
+++ app.py
@@ -46,6 +46,8 @@ st.write("""
          pairs using their HA1 sequences and (optional) metadata information.
          ***
          """)
+st.subheader("1 - Select season from left sidebar")
+st.subheader("2 - Select input type from left sidebar")
 
 
 #%%
